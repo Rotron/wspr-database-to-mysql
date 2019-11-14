140ab9d6783bb697d46c0c7eff5f16159d96dd32
Federico Saccà <info@federicosacca.it>
2019-11-14 15:45:55 +0000

    Update wspr.sql

--- wspr.sql
+++ wspr.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : Mysql
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : wspr
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 14/11/2019 15:19:05
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
